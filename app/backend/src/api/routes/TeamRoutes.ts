7dc5f80d65efa7e41bcee3d6a7a616094668eb63
Thiago Pereira <titiura@gmail.com>
2023-03-10 20:51:21 -0300

    fix: teamRoutes - erro na escrita da rota

--- app/backend/src/api/routes/TeamRoutes.ts
+++ app/backend/src/api/routes/TeamRoutes.ts
@@ -7,6 +7,6 @@ const teamService = new TeamService();
 const teamController = new TeamController(teamService);
 
 teamRoutes.get('/teams', (req: Request, res: Response) => teamController.readAllUser(req, res));
-teamRoutes.get('/team/:id', (req: Request, res: Response) => teamController.readById(req, res));
+teamRoutes.get('/teams/:id', (req: Request, res: Response) => teamController.readById(req, res));
 
 export default teamRoutes;
