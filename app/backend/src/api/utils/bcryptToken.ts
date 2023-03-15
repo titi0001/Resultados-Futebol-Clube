03bb5459c8ea023ea8a0ad5bb0386ae12e5fdca3
Thiago Pereira <titiura@gmail.com>
2023-03-15 18:51:46 -0300

    fix: teste ESLint trybe

--- app/backend/src/api/utils/bcryptToken.ts
+++ app/backend/src/api/utils/bcryptToken.ts
@@ -1,6 +1,6 @@
-import { hash } from 'bcryptjs';
+// import { hash } from 'bcryptjs';
 
-export default async function createHashPassword(password: string):Promise<string> {
-  const passwordHash = await hash(password, 10);
-  return passwordHash;
-}
+// export default async function createHashPassword(password: string):Promise<string> {
+//   const passwordHash = await hash(password, 10);
+//   return passwordHash;
+// }
